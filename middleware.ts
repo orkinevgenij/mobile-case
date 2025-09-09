8b9d0070704bf1d5b6e261d3296072119ddbde08
evgenij <orkinevgenij@gmail.com>
2025-09-09 21:14:51 +0300

    fix middleware1

--- middleware.ts
+++ middleware.ts
@@ -1,35 +1,27 @@
-// middleware.ts
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 import { privateRoutes } from './routes';
 
-export async function middleware(req: NextRequest) {
+export async function middleware(req: any) {
   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
   const { pathname } = req.nextUrl;
   const isLoggedIn = !!token;
   const role = token?.role;
-  const baseUrl = process.env.BASE_URL || '';
+  const baseUrl = process.env.BASE_URL!;
 
   const isPrivateRoute = privateRoutes.includes(pathname);
   const isAdminRoute = pathname.startsWith('/dashboard');
-  const isAuthRoute = pathname.startsWith('/auth');
-  const isApiRoute = pathname.startsWith('/api');
 
-  if (isApiRoute) {
-    return NextResponse.next();
-  }
-
-  if (isLoggedIn && isAuthRoute) {
-    return NextResponse.redirect(`${baseUrl}/`);
-  }
-
-  if (!isLoggedIn && (isPrivateRoute || isAdminRoute)) {
+  if (isPrivateRoute && !isLoggedIn) {
     return NextResponse.redirect(`${baseUrl}/auth/login`);
   }
 
   if (isAdminRoute && role !== 'ADMIN') {
-    return NextResponse.redirect(`${baseUrl}/`);
+    return NextResponse.redirect(`${baseUrl}`);
+  }
+
+  if (isLoggedIn && pathname.startsWith('/auth')) {
+    return NextResponse.redirect(`${baseUrl}`);
   }
 
   return NextResponse.next();
