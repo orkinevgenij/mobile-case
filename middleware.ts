1d46dcb658ace402a037b5b93ffbfe95bc8fd210
evgenij <orkinevgenij@gmail.com>
2025-10-20 18:50:46 +0300

    refactor middleware

--- middleware.ts
+++ middleware.ts
@@ -8,24 +8,21 @@ const { auth } = NextAuth(authConfig);
 export default auth(async (req) => {
   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
   const { nextUrl } = req;
+
   const isLoggedIn = !!req.auth;
   const role = token?.role;
   const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL!;
-  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
-  const isApiRoute = nextUrl.pathname.includes('/api');
-  const isAuthRoute = nextUrl.pathname.includes('/auth');
-  const isAdminRoute = nextUrl.pathname.startsWith('/dashboard');
-  console.log('isAdmin', isAdminRoute);
-  if (isApiRoute) {
-    return;
-  }
+  const pathname = nextUrl.pathname;
+
+  const isPrivateRoute = privateRoutes.includes(pathname);
+  const isApiRoute = pathname.startsWith('/api');
+  const isAuthRoute = pathname.startsWith('/auth');
+  const isAdminRoute = pathname.startsWith('/dashboard');
+
+  if (isApiRoute) return;
 
   if (isLoggedIn && isAuthRoute) {
     return Response.redirect(`${NEXT_PUBLIC_BASE_URL}`);
-  }
-
-  if (isAuthRoute && !isLoggedIn) {
-    return;
   }
 
   if (isPrivateRoute && !isLoggedIn) {
@@ -33,10 +30,12 @@ export default auth(async (req) => {
   }
 
   if (isAdminRoute) {
-    if (role !== 'ADMIN') return Response.redirect(`${NEXT_PUBLIC_BASE_URL}`);
+    if (isLoggedIn && !role && token?.email) {
+      return;
+    }
+
+    if (role !== 'ADMIN') {
+      return Response.redirect(`${NEXT_PUBLIC_BASE_URL}`);
+    }
   }
 });
-
-export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-};
