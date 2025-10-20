c89ecd542bd7b3ba327a53b120eb8bac004b9628
evgenij <orkinevgenij@gmail.com>
2025-10-20 18:30:39 +0300

    refactor auth

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import NextAuth, { DefaultSession } from 'next-auth';
 import authConfig from './auth.config';
 import { prisma } from './lib/prisma';
 import { getUserByEmail } from './lib/user';
+
 declare module 'next-auth' {
   interface Session {
     user: {
@@ -11,6 +12,7 @@ declare module 'next-auth' {
     } & DefaultSession['user'];
   }
 }
+
 export const {
   handlers: { GET, POST },
   auth,
@@ -26,6 +28,28 @@ export const {
     async signIn({ user, account }) {
       return true;
     },
+
+    async jwt({ token, user }) {
+      // 🔹 1️⃣
+      if (user) {
+        const u = user as any;
+        token.email = u.email;
+        token.role = u.role;
+        token.userId = u.id;
+        return token;
+      }
+
+      if (token.email) {
+        const dbUser = await getUserByEmail(token.email);
+        if (dbUser) {
+          token.role = dbUser.role;
+          token.userId = dbUser.id;
+        }
+      }
+
+      return token;
+    },
+
     async session({ token, session }) {
       if (token.role) {
         session.user.role = token.role as 'USER' | 'ADMIN';
@@ -34,15 +58,6 @@ export const {
         session.user.userId = token.userId as string;
       }
       return session;
-    },
-    async jwt({ token }) {
-      if (!token.email) return token;
-      const user = await getUserByEmail(token.email);
-      if (!user) return token;
-
-      token.role = user.role;
-      token.userId = user.id;
-      return token;
     },
   },
 });
