0be1b5819f78dcced36bffa179de6f2ca166cadc
evgenij <orkinevgenij@gmail.com>
2025-10-27 19:26:06 +0200

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -6,15 +6,19 @@ import { privateRoutes } from './routes';
 const { auth } = NextAuth(authConfig);
 
 export default auth(async (req) => {
+  console.log('req', req);
   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  console.log('token', token);
   const role = token?.role;
+  console.log('role', role);
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
   const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL!;
+  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
   const isApiRoute = nextUrl.pathname.includes('/api');
   const isAuthRoute = nextUrl.pathname.includes('/auth');
-  const isPrivateRoute = nextUrl.pathname.startsWith('/orders');
   const isAdminRoute = nextUrl.pathname.startsWith('/dashboard');
+  console.log('isAdmin', isAdminRoute);
 
   if (isApiRoute) {
     return;
@@ -32,8 +36,10 @@ export default auth(async (req) => {
     return Response.redirect(`${NEXT_PUBLIC_BASE_URL}/auth/login`);
   }
 
-  if (isAdminRoute && role !== 'ADMIN') {
-    return Response.redirect(`${NEXT_PUBLIC_BASE_URL}`);
+  if (isAdminRoute) {
+    if (role !== 'ADMIN') {
+      return Response.redirect(`${NEXT_PUBLIC_BASE_URL}`);
+    }
   }
 });
 
